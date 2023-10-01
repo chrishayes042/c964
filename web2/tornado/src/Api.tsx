93a8764c5fa87c56606316a8cc84d1303bfcb44d
chris <chrishayes042@gmail.com>
2023-10-01 12:09:11 -0600

    restored local api calls

--- web2/tornado/src/Api.tsx
+++ web2/tornado/src/Api.tsx
@@ -8,43 +8,37 @@ import {
 // localhost:3333
 // chrishayesc964.wafflecoder.net:3333
 export async function getAllData(): Promise<Tornado[]> {
-  const url = `http://chrishayesc964.wafflecoder.net:3333/api/readAllData`;
+  const url = `http://localhost:3333/api/readAllData`;
   const res = await fetch(url);
   const tornado: Tornado[] = await res.json();
   return tornado;
 }
 export async function getTornadoData(): Promise<TornadoReport[]> {
-  const res = await fetch(
-    `http://chrishayesc964.wafflecoder.net:3333/api/readAllData`
-  );
+  const res = await fetch(`http://localhost:3333/api/readAllData`);
   const torn: TornadoReport[] = await res.json();
   return torn;
 }
 export async function getTornadoDecadeData(): Promise<TornadoDecadeData[]> {
-  const res = await fetch(
-    "http://chrishayesc964.wafflecoder.net:3333/api/getDecadeData"
-  );
+  const res = await fetch("http://localhost:3333/api/getDecadeData");
   const decadeData: TornadoDecadeData[] = await res.json();
   return decadeData;
 }
 export async function getPredictData(): Promise<TornadoDecadeData[]> {
-  const res = await fetch(
-    "http://chrishayesc964.wafflecoder.net:3333/api/getPredictData"
-  );
+  const res = await fetch("http://localhost:3333/api/getPredictData");
   const predData: TornadoDecadeData[] = await res.json();
   return predData;
 }
 
 export async function getStateData(state: string): Promise<Tornado[]> {
   const res = await fetch(
-    `http://chrishayesc964.wafflecoder.net:3333/api/getStateData?state=${state}`
+    `http://localhost:3333/api/getStateData?state=${state}`
   );
   const torn: Tornado[] = await res.json();
   return torn;
 }
 export async function getStateTotalData(state: string): Promise<StateTotals> {
   const res = await fetch(
-    `http://chrishayesc964.wafflecoder.net:3333/api/getStateTotals?state=${state}`
+    `http://localhost:3333/api/getStateTotals?state=${state}`
   );
   const torn: StateTotals = await res.json();
   return torn;
@@ -53,7 +47,7 @@ export async function getStateDecadeTotalData(
   state: string
 ): Promise<StateDecadeTotals[]> {
   const res = await fetch(
-    `http://chrishayesc964.wafflecoder.net:3333/api/getStateDecadeTotals?state=${state}`
+    `http://localhost:3333/api/getStateDecadeTotals?state=${state}`
   );
   const stateTotals: StateDecadeTotals[] = await res.json();
   return stateTotals;
@@ -62,15 +56,13 @@ export async function getStatePredictionData(
   state: string
 ): Promise<TornadoPrediction[]> {
   const res = await fetch(
-    `http://chrishayesc964.wafflecoder.net:3333/api/getPredictionTotals?state=${state}`
+    `http://localhost:3333/api/getPredictionTotals?state=${state}`
   );
   const totals: TornadoPrediction[] = await res.json();
   return totals;
 }
 export async function getStateResiduals(): Promise<TornadoResidual[]> {
-  const res = await fetch(
-    `http://chrishayesc964.wafflecoder.net:3333/api/getAllResiduals`
-  );
+  const res = await fetch(`http://localhost:3333/api/getAllResiduals`);
   const totals: TornadoResidual[] = await res.json();
   return totals;
 }
